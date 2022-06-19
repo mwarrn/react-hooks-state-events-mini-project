47fad1692656797de2753c6accf36d41d7d5e007
Michael Warren <mwarrn@amazon.com>
2022-06-18 17:57:46 -0700

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,27 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState([...TASKS]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function onTaskFormSubmit(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory} />
+      <NewTaskForm 
+        categories={CATEGORIES}
+        onTaskFormSubmit={onTaskFormSubmit} />
+      <TaskList
+        tasks={tasks}
+        setTasks={setTasks}
+        selectedCategory={selectedCategory} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,33 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuid } from "uuid";
 
-function TaskList() {
+function TaskList({ tasks, setTasks, selectedCategory }) {
+  let tasksToDisplay = [...tasks];
+
+  if (selectedCategory !== "All") {
+    tasksToDisplay = tasks.filter(task => task.category === selectedCategory ? true : false);
+  }
+
+  function handleOnDelete(toDelete) {
+    const deletedTask = tasks.filter(task => task.text !== toDelete.text ? true : false);
+    setTasks(deletedTask);
+  }
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasksToDisplay.map((task) => {
+        return (
+          <Task
+          key={uuid()}
+          task={task}
+          text={task.text}
+          category={task.category}
+          onDelete={handleOnDelete}
+          />
+        )}
+      )}
     </div>
   );
 }
